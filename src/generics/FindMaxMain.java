9cab986566b1658ce4611456b0f4173bf012f1d9
Veer1498 <singavarapuveerendra@gmail.com>
2022-08-26 23:37:13 +0530

    [veer] Added : Ability to Find Maximum in Integers Values

--- src/generics/FindMaxMain.java
+++ src/generics/FindMaxMain.java
@@ -6,6 +6,10 @@ public class FindMaxMain {
 		// TODO Auto-generated method stub
 		FindMaximum findMax = new FindMaximum();
 		findMax.welcome();
+		System.out.println("\nINTEGERS");
+		findMax.getMaxInteger(10, 5, 35);
+//		System.out.println("\nFLOAT");
+//		findMax.getMaxFloat(1.2f, 2.2f, 1.1f);
 	}
 
 }
--- src/generics/FindMaximum.java
+++ src/generics/FindMaximum.java
@@ -5,4 +5,31 @@ public class FindMaximum {
 		System.out.println("Welcome to Find maximum Using Generics Program");
 		System.out.println("==============================================");
 	}
+	
+	public Integer getMaxInteger(Integer x,Integer y,Integer z) {
+		Integer max = x;
+		if(y.compareTo(x) > 0 && y.compareTo(z) > 0) {
+			max = y;
+		}
+		else if(z.compareTo(x) > 0 && z.compareTo(y) > 0) {
+			max = z;
+		}
+		System.out.println("Values : "+x+" "+y+" "+z);
+		System.out.println("The Max of 3 Values : "+max);
+		return max;
+		
+	}
+	
+//	public Float getMaxFloat(Float x, Float y, Float z) {		
+//		Float max = x;
+//		if(y.compareTo(x)>0 && y.compareTo(z)>0) {
+//			max = y;
+//		}
+//		else if(z.compareTo(y)>0 && z.compareTo(x)>0) {
+//			max = z;
+//		}
+//		System.out.println("Values : "+x+" "+y+" "+z);
+//		System.out.println("The Max is "+max);
+//		return max;
+//	}
 }
