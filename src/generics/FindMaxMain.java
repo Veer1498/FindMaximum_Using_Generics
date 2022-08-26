63cf6fe9459d42113a9ec3b379c50b2ddba7cf1c
Veer1498 <singavarapuveerendra@gmail.com>
2022-08-26 22:58:47 +0530

    [veer] Added : Welcome Message

---
+++ src/generics/FindMaxMain.java
@@ -0,0 +1,11 @@
+package generics;
+
+public class FindMaxMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FindMaximum findMax = new FindMaximum();
+		findMax.welcome();
+	}
+
+}
---
+++ src/generics/FindMaximum.java
@@ -0,0 +1,8 @@
+package generics;
+
+public class FindMaximum {
+	public void welcome() {
+		System.out.println("Welcome to Find maximum Using Generics Program");
+		System.out.println("==============================================");
+	}
+}
